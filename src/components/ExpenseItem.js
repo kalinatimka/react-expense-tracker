14597d8b7bb186313a329839164bebdcd27179d3
Tsimafei Kalinouski <tsimafei_kalinouski@epam.com>
2023-07-30 15:18:48 +0300

    feat: modify way of passing props data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  const { title, amount, date} = props;
-
+function ExpenseItem({ expense }) {
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>{expense.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${amount}</div>
+        <h2>{expense.title}</h2>
+        <div className="expense-item__price">${expense.amount}</div>
       </div>
     </div>
   );
