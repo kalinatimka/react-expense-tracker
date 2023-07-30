3de247ec5b6213af88bd5eb7a7758a71d270fd0d
Tsimafei Kalinouski <tsimafei_kalinouski@epam.com>
2023-07-30 15:42:39 +0300

    feat: change date format in ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ function App() {
       id: "id1",
       title: "Toilet Paper",
       amount: 1.35,
-      date: new Date(2023, 7, 25),
+      date: new Date(2023, 4, 22),
     },
     {
       id: "id1",
       title: "New TV",
       amount: 431.28,
-      date: new Date(2023, 7, 25),
+      date: new Date(2023, 5, 23),
     },
     {
       id: "id1",
       title: "Car Insurance",
       amount: 123,
-      date: new Date(2023, 7, 25),
+      date: new Date(2023, 6, 24),
     },
     {
       id: "id1",
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem({ expense }) {
+  const expenseMonth = expense.date.toLocaleString('en-US', { month: 'long' });
+  const expenseYear = expense.date.toLocaleString('en-US', { day: '2-digit' });;
+  const expenseDay = expense.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{expense.date.toISOString()}</div>
+      <div>
+        <div>{expenseMonth}</div>
+        <div>{expenseYear}</div>
+        <div>{expenseDay}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
