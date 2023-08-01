7f0d2300dc864bfa8d2c16feb78db6520fb51984
Tsimafei Kalinouski <tsimafei_kalinouski@epam.com>
2023-08-01 16:16:16 +0300

    feat: move block with date into separate component

--- src/App.js
+++ src/App.js
@@ -30,18 +30,10 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        expense={expenses[0]}
-      ></ExpenseItem>
-      <ExpenseItem
-        expense={expenses[1]}
-      ></ExpenseItem>
-      <ExpenseItem
-        expense={expenses[2]}
-      ></ExpenseItem>
-      <ExpenseItem
-        expense={expenses[3]}
-      ></ExpenseItem>
+      <ExpenseItem expense={expenses[0]}></ExpenseItem>
+      <ExpenseItem expense={expenses[1]}></ExpenseItem>
+      <ExpenseItem expense={expenses[2]}></ExpenseItem>
+      <ExpenseItem expense={expenses[3]}></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ expense }) {
-  const expenseMonth = expense.date.toLocaleString('en-US', { month: 'long' });
-  const expenseYear = expense.date.toLocaleString('en-US', { day: '2-digit' });;
-  const expenseDay = expense.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{expenseMonth}</div>
-        <div>{expenseYear}</div>
-        <div>{expenseDay}</div>
-      </div>
+      <ExpenseDate date={expense.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{expense.title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
