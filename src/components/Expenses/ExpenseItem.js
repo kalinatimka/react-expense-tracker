a6a6cbddc777bd21f7c8f69a2baf6f73e4745cfe
Tsimafei Kalinouski <tsimafei_kalinouski@epam.com>
2023-10-03 16:53:15 +0300

    feat: add state to application

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
+import { useState } from "react";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem({ expense }) {
+  const [title, setTitle] = useState(expense.title);
+
+  const onClickHandler = () => {
+    setTitle('Another title');
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={expense.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{expense.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${expense.amount}</div>
       </div>
+      <button onClick={onClickHandler}>Click me!</button>
     </Card>
   );
 }
