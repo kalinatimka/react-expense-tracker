651e1f7c23d81c10d04f197ff2ae567d6c413df9
Tsimafei Kalinouski <tsimafei_kalinouski@epam.com>
2023-10-04 22:09:46 +0300

    feat: add ExpensesFilter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../ExpensesFilter/ExpensesFilter";
+
 import "./Expenses.css";
+import { useState } from "react";
 
 function Expenses({ expenses }) {
+  const [ year, setYear ] = useState('2021');
+
+  const yearChangeHandler = (year) => {
+    setYear(year);
+    console.log(year);
+  }
+
   return (
     <Card className="expenses">
+      <ExpensesFilter year={year} onYearChange={yearChangeHandler}></ExpensesFilter>
       <ExpenseItem expense={expenses[0]}></ExpenseItem>
       <ExpenseItem expense={expenses[1]}></ExpenseItem>
       <ExpenseItem expense={expenses[2]}></ExpenseItem>
